6856e673b7e5389106d14fca20c73713ef6ae754
Codeologynerd <49155095+Codeologynerd@users.noreply.github.com>
2020-05-13 22:22:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,72 @@
-var slideIndex = 2;
-showSlides(slideIndex);
+var ul;
+var liItems; 
+var imageWidth;
+var imageNumber;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function init(){
+
+    ul = document.getElementById(‘image_slider’);
+    liItems = ul.children;
+    imageNumber = liItems.length;
+    imageWidth = liItems[0].children[0].offsetWidth;
+    // set ul’s width as the total width of all images in image slider.
+    ul.style.width = parseInt(imageWidth * imageNumber) + ‘px’;
+    slider(ul);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function slider(ul){ 
+    animate({
+        delay:17,
+        duration: 3000,
+        delta:function(p){return Math.max(0, -1 + 2 * p)},
+        step:function(delta){
+            ul.style.left = ‘-’ + parseInt(currentImage * imageWidth + delta * imageWidth) + ‘px’;
+    },
+        callback:function(){
+            currentImage++;
+        // if it doesn’t slied to the last image, keep sliding
+            if(currentImage < imageNumber-1){
+                slider(ul);
+        }
+       // if current image it’s the last one, it slides back to the first one
+            else{
+                var leftPosition = (imageNumber - 1) * imageWidth;
+               // after 2 seconds, call the goBack function to slide to the first image 
+                setTimeout(function(){goBack(leftPosition)},2000); 
+                setTimeout(function(){slider(ul)}, 4000);
+            }
+        }
+    });
 }
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+function goBack(leftPosition){
+    currentImage = 0; 
+    var id = setInterval(function(){
+        if(leftPosition >= 0){
+            ul.style.left = ‘-’ + parseInt(leftPosition) + ‘px’;
+            leftPosition -= imageWidth / 10;
+        }
+        else{
+            clearInterval(id);
+        } 
+    }, 17);
 }
-
-$(window).on("load resize ", function() {
-  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-  $('.tbl-header').css({'padding-right':scrollWidth});
-}).resize()
-
+function animate(opts){
+    var start = new Date;
+    var id = setInterval(function(){
+        var timePassed = new Date - start;
+        var progress = timePassed / opts.duration
+        if(progress > 1){
+            progress = 1;
+        }
+        var delta = opts.delta(progress);
+        opts.step(delta);
+        if (progress == 1){
+            clearInterval(id);
+           opts.callback();
+         }
+    }, opts.dalay || 17);
+}
+window.onload = init;
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
