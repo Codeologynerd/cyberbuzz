34d507e64d77d43cc378a9efb68ec76faab43f13
Codeologynerd <49155095+Codeologynerd@users.noreply.github.com>
2020-05-13 16:19:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,17 +12,68 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+$(window).on("load resize ", function() {
+  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+  $('.tbl-header').css({'padding-right':scrollWidth});
+}).resize()
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function my() {
+  var dot = document.getElementById("dot");
+  var bigText = document.getElementById("big");
+  var btnText = document.getElementById("myBtn");
+
+  if ( dot.style.display === "none") {
+    dot.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    bigText.style.display = "none";
+  } else {
+    dot.style.display = "none";
+    btnText.innerHTML = "Read less";
+    bigText.style.display = "inline";
+  }
+}
+
+function MY() {
+  var dot1 = document.getElementById("dot1");
+  var largeText = document.getElementById("large");
+  var btnText = document.getElementById("myBtn");
+
+  if ( dot1.style.display === "none") {
+    dot1.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    largeText.style.display = "none";
+  } else {
+    dot1.style.display = "none";
+    btnText.innerHTML = "Read less";
+    largeText.style.display = "inline";
+  }
 }
